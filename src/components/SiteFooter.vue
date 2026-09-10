8532fb537b15df78cb494bb2207ac440cd3b1ac5
klldh <940271269@qq.com>
2026-09-10 18:03:55 +0800

    modify: replace the ICP filing link with public security registration and emblem

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,4 +1,6 @@
 <script setup>
+    import nationalEmblem from '../assets/icons/national-emblem.png'
+
     const currentYear = new Date().getFullYear()
 </script>
 
@@ -14,8 +16,11 @@
                 <p>水暖器材　卫浴洁具<br>水疗设备　管道配件</p></div>
         </div>
         <div class="copyright">
-            © {{ currentYear }} 求精（宁波）水暖器材厂 ·
-            <a href="https://beian.miit.gov.cn/" target="_blank" rel="noopener noreferrer">浙ICP备2026073096号</a>
+            <span>© {{ currentYear }} 求精（宁波）水暖器材厂</span>
+            <a class="beian-link" href="https://beian.mps.gov.cn/#/query/webSearch?code=33020302002226" target="_blank" rel="noopener noreferrer">
+                <img :src="nationalEmblem" alt="" width="18" height="20">
+                <span>浙公网安备33020302002226号</span>
+            </a>
         </div>
     </footer>
 </template>
